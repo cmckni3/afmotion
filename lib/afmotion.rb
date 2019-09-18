67b5bc01b9f74b50141428b69440c08ba81cab3d
Chris McKnight <cmckni3@gmail.com>
2019-09-18 13:59:40 -0500

    Include project files without motion-require

--- lib/afmotion.rb
+++ lib/afmotion.rb
@@ -5,13 +5,12 @@ unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
 
-Motion::Require.all(Dir.glob(File.join(File.dirname(__FILE__), 'afmotion/**/*.rb')))
-
 Motion::Project::App.setup do |app|
-  Dir.glob(File.join(File.dirname(__FILE__), 'afmotion/**/*.rb')).each do |file|
-    if app.respond_to?("exclude_from_detect_dependencies")
-      app.exclude_from_detect_dependencies << file
-    end
+  core_lib = File.join(File.dirname(__FILE__), 'afmotion')
+  insert_point = app.files.find_index { |file| file =~ /^(?:\.\/)?app\// } || 0
+
+  Dir.glob(File.join(core_lib, '**/*.rb')).reverse.each do |file|
+    app.files.insert(insert_point, file)
   end
 
   app.pods do
